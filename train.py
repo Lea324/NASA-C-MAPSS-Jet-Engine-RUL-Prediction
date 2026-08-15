d40e7cf38dd69a1f3a182b04001ebbb676dfbe39
Lea324 <ll324@ic.ac.uk>
2026-08-15 05:33:26 +0100

    push

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
+import time
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 # Load the training data
 df = pd.read_csv('C:\\Jet machine learning\\Aircraft Engine Dataset\\FD001\\train.csv')
 # Preprocess the data
@@ -17,12 +19,33 @@ df = df.drop_duplicates()
 #------------------Actually, there are no duplicate rows in the dataset--------
 #print(df["unit_number"].nunique()) # it tell me it have 100 unit_number
 #===============================================================================
-
 #Input features
-X = df.drop(['unit_number','max_cycle', 'RUL','health_stage'], axis=1)
+X = df.drop(['max_cycle', 'RUL','health_stage'], axis=1)
 Y = df['RUL']
-
+start_time = time.time()
 # Implementing the linear regression model
 model = LinearRegression()
 model.fit(X, Y) #the training step
-print("Model trained successfully!")
+df_test = pd.read_csv('C:\\Jet machine learning\\Aircraft Engine Dataset\\FD001\\test.csv')
+df_test_last = df_test.loc[
+    df_test.groupby('unit_number')['time_in_cycles'].idxmax()
+] #only keep the last cycle of each unit_number
+X_test = df_test_last[X.columns]
+Y_pred = model.predict(X_test)
+end_time = time.time()
+
+#obtain the true RUL values from the RUL_targets.csv file
+df_target = pd.read_csv('C:\\Jet machine learning\\Aircraft Engine Dataset\\FD001\\RUL_targets.csv')
+Y_test = df_target['true_RUL'].astype(float).to_numpy()
+#print("X_test shape:", X_test.shape)
+#print("Y_pred shape:", Y_pred.shape)
+#print("Y_test shape:", Y_test.shape)
+
+#==================== Calculate errors==========================
+mae = mean_absolute_error(Y_test, Y_pred)
+rmse = np.sqrt(mean_squared_error(Y_test, Y_pred))
+r2 = r2_score(Y_test, Y_pred)
+print("MAE:", format(mae, ".4g"))
+print("RMSE:", format(rmse, ".4g"))
+print("R²:", format(r2, ".4g"))
+print(f"Computational Time: {format(end_time - start_time, '.4g')} seconds")
